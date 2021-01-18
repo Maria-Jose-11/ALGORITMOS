255c0a3b440d3215ce227b8ffacd6d413ffcdbfb
Maria-Jose-11 <mariajochala@gmail.com>
2021-01-18 13:27:14 -0500

    DEBER N. 16

---
+++ Triangulo/PROGRAMA_TRIÁNGULO_RECTÁNGULO.cpp
@@ -0,0 +1,51 @@
+// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+// Es posible que el codigo generado no sea completamente correcto. Si encuentra
+// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	float area;
+	float cat1;
+	float cat2;
+	float hip;
+	float l1;
+	float l2;
+	float l3;
+	cout << "Ingrese el lado 1:" << endl;
+	cin >> l1;
+	cout << "Ingrese el lado 2:" << endl;
+	cin >> l2;
+	cout << "Ingrese el lado 3:" << endl;
+	cin >> l3;
+	if (l1>l2) {
+		cat1 = l2;
+		if (l1>l3) {
+			hip = l1;
+			cat2 = l3;
+		} else {
+			hip = l3;
+			cat2 = l1;
+		}
+	} else {
+		cat1 = l1;
+		if (l2>l3) {
+			hip = l2;
+			cat2 = l3;
+		} else {
+			hip = l3;
+			cat2 = l2;
+		}
+	}
+	if (pow(hip,2)==pow(cat1,2)+pow(cat2,2)) {
+		area = (cat1*cat2)/2;
+		cout << "Si es un triángulo rectangulo" << endl;
+		cout << "El area es: " << area << endl;
+	} else {
+		cout << "No es un triangulo rectangulo." << endl;
+	}
+	return 0;
+}
+
